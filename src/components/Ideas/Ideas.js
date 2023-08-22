820c2136a8ac681ea3daeac460526d74ceebf5a7
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-22 15:40:18 -0600

    Add Card component to render cards

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,11 +1,17 @@
 import './Ideas.css'
+import Card from '../Card/Card'
 
 function Ideas(props) {
   const {name} = props
   return (
-    <div>
-      <h2>Hello, {name}!</h2>
-      <h2>My phone # is {props.phone}</h2>
+    // <div>
+    //   <h2>Hello, {name}!</h2>
+    //   <h2>My phone # is {props.phone}</h2>
+    // </div>
+    <div className='ideas-container'>
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
