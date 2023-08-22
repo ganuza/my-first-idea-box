06eb5ddc72b2f5383808a0d7ac747a03fc8eb852
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-22 13:26:10 -0600

    Add three dummy ideas to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+  ]
+  const [ideas, setIdeas] = useState(dummyIdeas)
 
-  const [ideas, setIdea] = useState([])
-
+  console.log(ideas)
   return (
     <main className='App'>
       <h1>IdeaBox</h1>
